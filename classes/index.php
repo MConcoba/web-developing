024cc6ed4872ed47e6787bfa018466e5a8ff2870
mconcoba <mconcoba20@gmail.com>
2024-08-16 18:45:51 -0600

    feat(add): restucture files in classes

--- classes/index.php
+++ classes/index.php
@@ -1,9 +1,2 @@
 <?php
-
-    echo '<h1  style="text-align: center;">Desarrollo web</h1> <br>';
-    
-
-    $num = 10;
-    $num = 10.5;
-
-    echo 'El valor es ' .$num . '<br>';
+include("./classes/02-view-funciones.php");
