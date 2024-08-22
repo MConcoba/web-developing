f227d4a405a8ef27507933adf77d3ee24b74131f
mconcoba <mconcoba20@gmail.com>
2024-08-22 01:22:26 -0600

    feat(add): config get data of file config

--- homeworks/conexion_db_pdo/conexion.php
+++ homeworks/conexion_db_pdo/conexion.php
@@ -1,19 +1,20 @@
 <?php
+include __DIR__ . '../../../config.php';
+$env = env();
 
-
-
+// Funcion para crear la conexion
 function conexionDb()
 {
-    $dsn = 'pgsql:host=localhost;port=5432;dbname=umg_postgres'; // Reemplaza con tu host, puerto y nombre de la base de datos
-    $username = 'postgres'; // Reemplaza con tu usuario de PostgreSQL
-    $password = 'password'; // Reemplaza con tu contraseña de PostgreSQL
+    global $env;
+    $dsn = "mysql:host={$env['db_host']};dbname={$env['db_name']}";
+    $username = $env['db_user'];
+    $password = $env['db_pass'];
 
     try {
         // Crear una nueva conexión PDO
         $pdo = new PDO($dsn, $username, $password);
         // Establecer el modo de error de PDO a excepción
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo "Conexión exitosa a PostgreSQL!";
         return [
             'status' => true,
             'message' => '',
@@ -21,7 +22,6 @@ function conexionDb()
         ];
     } catch (PDOException $e) {
         // Mostrar el error si la conexión falla
-        //echo "Conexión fallida: " . $e->getMessage();
         return [
             'status' => false,
             'message' => $e->getMessage()
@@ -29,18 +29,17 @@ function conexionDb()
     }
 }
 
+// Funcion para ejecutar un query
 function runQuery($query, $db)
 {
     try {
         $run = $db->query($query);
         $results = $run->fetchAll(PDO::FETCH_ASSOC);
-        //return $results;
         return [
             'status' => true,
             'message' => $results
         ];
     } catch (Throwable $th) {
-        //echo $th->getMessage();
         return [
             'status' => false,
             'message' => $th->getMessage()
