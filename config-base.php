169f7c999ef4812728413aaba1265d89698b1811
mconcoba <mconcoba20@gmail.com>
2024-08-22 01:22:07 -0600

    feat(add): config to enviroment structure

---
+++ config-base.php
@@ -0,0 +1,12 @@
+<?php
+
+function env()
+{
+    $config = [
+        'db_host' => 'localhost',
+        'db_name' => 'db',
+        'db_user' => 'user',
+        'db_pass' => 'password',
+    ];
+    return $config;
+}
