7aa7b77d9bb334e2529e69aa74fb59d7e18c88db
mconcoba <mconcoba20@gmail.com>
2024-09-12 11:19:47 -0600

    feat(add): class of javascript

---
+++ classes/javascript/js/01.js
@@ -0,0 +1,15 @@
+// SELECCION POR ID
+
+const title = document.getElementById('title');
+const parrafo1 = document.getElementById('parrafo1');
+const parrafo2 = document.getElementById('parrafo2');
+const parrafo3 = document.getElementById('parrafo3');
+
+console.log(parrafo1.textContent);
+console.log(parrafo2.textContent);
+console.log(parrafo3.textContent);
+
+const parrafos = document.getElementsByClassName('parrafos');
+
+const t = document.getElementsByTagName('p');
+console.log(t);
